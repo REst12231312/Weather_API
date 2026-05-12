49cc9fb660c11fab250c1f1c10eb4419226d2206
Andrey <varvaryuk.ag503215@students.tou.edu.kz>
2026-05-12 12:58:21 +0500

    fixed problems by rendering

--- src/components/WeatherPage/WeatherWeek.jsx
+++ src/components/WeatherPage/WeatherWeek.jsx
@@ -17,8 +17,9 @@ export default function WeatherWeek({ getWeatherWeek, viewWeatherWeek }) {
 
     console.log(viewWeatherWeek);
 
-    const groupedWeather = viewWeatherWeek.list.reduce((acc, item) => {
-        const date = item.dt_txt.splice(' ')[0]
+
+    const groupedWeather = viewWeatherWeek?.list?.reduce((acc, item) => {
+        const date = item.dt_txt.split(' ')[0]
 
         if (!acc[date]) {
             acc[date] = []
@@ -29,7 +30,23 @@ export default function WeatherWeek({ getWeatherWeek, viewWeatherWeek }) {
         return acc
     }, {})
 
+    console.log(groupedWeather);
+    
+
+    const serchedWeather = Object.entries(groupedWeather || {}).map(([date, items]) => {
+        const midday = items.find(i => i.dt_txt.includes("12:00:00") || items[Math.floor(items.length / 2) || items[0]])
+
+        return(
+            <div key={date}>
+                <h2>{date}</h2>
+                <div>
+                    <p>{Math.floor(midday?.main.temp)}°C</p>
+                </div>
+            </div>
+        )
+    })
+
     return (
-        <div>WeatherWeek</div>
+        <div>{serchedWeather}</div>
     )
 }
--- src/components/api/ApiWeekWeather.js
+++ src/components/api/ApiWeekWeather.js
@@ -3,7 +3,7 @@ export const getApiWeekWeather = async () => {
     const API_KEY = "d6c61bc3bb842db769b77c72a4684c11"
 
     const res = await fetch(
-        `https://api.openweathermap.org/data/2.5/forecast?lat=52.28&lon=76.96&appid=${API_KEY}`
+        `https://api.openweathermap.org/data/2.5/forecast?lat=52.28&lon=76.96&appid=${API_KEY}&units=metric`
     )
 
     if (!res.ok) {
